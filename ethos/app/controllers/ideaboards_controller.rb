f3e77ea38aeefa087aac4ee3fc4e7b24dbec9948
Nathan Kluth <nlkluth@gmail.com>
2014-10-29 09:53:18 -0600

    stubbing ideaboard files

--- ethos/app/controllers/ideaboards_controller.rb
+++ ethos/app/controllers/ideaboards_controller.rb
@@ -1,4 +1,27 @@
 class IdeaboardsController < ApplicationController
   def index
+    @ideaboards = Ideaboard.all
+  end
+
+  def show
+    @ideaboard = Ideaboard.find(params[:id])
+  end
+
+  def new
+    @ideaboard = Ideaboard.new
+  end
+
+  def create
+    @ideaboard = Ideaboard.new(ideaboard_params)
+
+    if @ideaboard.save
+      redirect_to @ideaboard
+    else
+      render 'new'
+    end
+  end
+
+  def ideaboard_params
+    params.require(:ideaboard).permit(:title, :description)
   end
 end
