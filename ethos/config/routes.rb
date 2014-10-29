412cad29f028c000fed152fe70585d6f8e3fe8f5
Nathan Kluth <nlkluth@gmail.com>
2014-10-29 09:53:03 -0600

    made custom routes

--- ethos/config/routes.rb
+++ ethos/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+  end
+
   root to: 'ideaboards#index'
 
   resources :ideaboards
