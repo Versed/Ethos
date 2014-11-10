a178c0e83d0d15dbf73e91a8f5a094587d72ebb0
Nathan Kluth <nlkluth@gmail.com>
2014-11-09 22:44:21 -0700

    linking docs

--- ethos/app/helpers/application_helper.rb
+++ ethos/app/helpers/application_helper.rb
@@ -13,4 +13,13 @@ module ApplicationHelper
   def can_display_ideaboard?(ideaboard)
     signed_in? && !current_user.has_blocked?(ideaboard.user) || !signed_in?
   end
+
+  def ideaboard_document_link(status)
+    html = ""
+
+    if status.document && status.document.attachment?
+      html << link_to(@ideaboard.document.attachment_file_name, @ideaboard.document.attachment.url)
+      return html.html_safe
+    end
+  end
 end
