b8d3b8bc24f29c12b32357ba0c0e4136c4ffe5fe
Nathan Kluth <nlkluth@gmail.com>
2014-11-11 14:14:18 -0700

    fixing bugs

--- ethos/app/helpers/application_helper.rb
+++ ethos/app/helpers/application_helper.rb
@@ -33,6 +33,6 @@ module ApplicationHelper
 
   def avatar_profile_link(user, image_options={}, html_options={})
     avatar_url = user.avatar? ? user.avatar.url(:thumb) : nil
-    link_to(image_tag(avatar_url, image_options), profile_path(user.profile_name))
+    link_to(image_tag(avatar_url, image_options), profile_path(user.username))
   end
 end
