42cbfe0e6cf26032fdd2221e12d571df8a5f8d23
Nathan Kluth <nlkluth@gmail.com>
2014-11-14 08:34:12 -0700

    moving helpers

--- ethos/app/helpers/application_helper.rb
+++ ethos/app/helpers/application_helper.rb
@@ -21,16 +21,6 @@ module ApplicationHelper
     end
   end
 
-  def can_display_ideaboard?(ideaboard)
-    signed_in? && !current_user.has_blocked?(ideaboard.user) || !signed_in?
-  end
-
-  def ideaboard_document_link(status)
-    if status.document && status.document.attachment?
-      link_to(@ideaboard.document.attachment_file_name, @ideaboard.document.attachment.url)
-    end
-  end
-
   def avatar_profile_link(user, image_options={}, html_options={})
     avatar_url = user.avatar? ? user.avatar.url(:thumb) : nil
     link_to(image_tag(avatar_url, image_options), profile_path(user.username))
--- ethos/app/helpers/ideaboards_helper.rb
+++ ethos/app/helpers/ideaboards_helper.rb
@@ -1,2 +1,11 @@
 module IdeaboardsHelper
+  def can_display_ideaboard?(ideaboard)
+    signed_in? && !current_user.has_blocked?(ideaboard.user) || !signed_in?
+  end
+
+  def ideaboard_document_link(status)
+    if status.document && status.document.attachment?
+      link_to(@ideaboard.document.attachment_file_name, @ideaboard.document.attachment.url)
+    end
+  end
 end
