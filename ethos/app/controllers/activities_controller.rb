7e2a80a2e797e0fd94c553031a063b1af920c023
Nathan Kluth <nlkluth@gmail.com>
2014-11-19 09:37:59 -0700

    fixing typo

--- ethos/app/controllers/activities_controller.rb
+++ ethos/app/controllers/activities_controller.rb
@@ -1,12 +1,13 @@
 class ActivitiesController < ApplicationController
+  before_filter :authenticate_user!
   respond_to :html, :json
 
   def index
     params[:page] ||= 1
-    # collaborators_id = curent_user.@ideaboards.map do |ideaboard|
+    # collaborators_id = current_user.@ideaboards.map do |ideaboard|
       # ideaboard.collaborators.map(&:id)
     # end
-    @activities = Activity.for_user(curent_user, params)
+    @activities = Activity.for_user(current_user, params)
     respond_with @activities
   end
 end
