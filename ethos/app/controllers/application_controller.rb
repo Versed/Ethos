0448bb457d613fcca5eab47abc9548a0a20cda9f
Nathan Kluth <nlkluth@gmail.com>
2014-11-19 09:38:22 -0700

    show 404 when not navigating to a real page

--- ethos/app/controllers/application_controller.rb
+++ ethos/app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
   protected
 
   def configure_permitted_parameters
@@ -11,5 +13,9 @@ class ApplicationController < ActionController::Base
 
   def render_permission_error
     render file: 'public/500.html', status: :error, layout: false
+  end
+
+  def render_404
+    render file: 'public/404.html', status: :not_found
   end
 end
