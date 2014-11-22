6358471dc18ee8980c3ca1a6323928f2bd3dea37
Nathan Kluth <nlkluth@gmail.com>
2014-11-22 00:12:32 -0700

    cleaning up profile and links

--- ethos/app/controllers/profiles_controller.rb
+++ ethos/app/controllers/profiles_controller.rb
@@ -1,9 +1,10 @@
 class ProfilesController < ApplicationController
   def show
+    params[:page] ||= 1
     @user = User.find_by_username(params[:id])
 
     if @user
-      @ideaboards = @user.ideaboards.all
+      @ideaboards = Ideaboard.where(:user_id => @user.id).paginate(:page => params[:page], :per_page => 4).all
       render action: :show
     else
       render file: 'public/404', status: 404, formats: [:html]
--- ethos/app/helpers/application_helper.rb
+++ ethos/app/helpers/application_helper.rb
@@ -23,6 +23,6 @@ module ApplicationHelper
 
   def avatar_profile_link(user, image_options={}, html_options={})
     avatar_url = user.avatar? ? user.avatar.url(:thumb) : 'default_profile.png'
-    link_to(image_tag(avatar_url, image_options), profile_path(user.username), html_options)
+    link_to(image_tag(avatar_url, image_options) + user.context_name(current_user), profile_path(user.username), html_options)
   end
 end
